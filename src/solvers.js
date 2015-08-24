e46cf1666e89f12dc52668a785c0fbde88a148ca
stevenlundy <steven.lundy@gmail.com>
2015-08-24 15:16:58 -0700

    Wrote findNQueensSolution

--- src/solvers.js
+++ src/solvers.js
@@ -42,22 +42,56 @@ window.addRook = function(board, col){
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({'n' : n});
+  var solutionCount = addRookAndCount(board, 0, 0);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
+window.addRookAndCount = function(board, col, count){
+  var rows = board.rows();
+  if(board.hasAnyRowConflicts()) {
+    return count;
+  }
+  if (col >= rows.length){
+    return count + 1;
+  }
+  for (var i = 0; i < rows.length; i++) {
+    rows[i][col] = 1;
+    count = addRookAndCount(board, col + 1, count);
+    rows[i][col] = 0;
+  }
+  return count;
+};
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
-
+  var board = new Board({'n' : n});
+  var solution = addQueen(board, 0) || board.rows();
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
 
+window.addQueen = function(board, col){
+  var rows = board.rows();
+  if(board.hasAnyRowConflicts() || board.hasAnyMajorDiagonalConflicts() || board.hasAnyMinorDiagonalConflicts()) {
+    return false;
+  }
+  if (col >= rows.length){
+    return rows;
+  }
+  for (var i = 0; i < rows.length; i++) {
+    rows[i][col] = 1;
+    var result = addQueen(board, col + 1);
+    if(result) {
+      return result;
+    } else {
+      rows[i][col] = 0;
+    }
+  }
+};
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
